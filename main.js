122c27304da09aea17b63836a8c058f8694e8559
roguewww <rogue.c3c@gmail.com>
2024-07-22 18:26:49 +0800

    almost right

--- main.js
+++ main.js
@@ -10,9 +10,13 @@ import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPa
 import { GammaCorrectionShader } from "three/examples/jsm/shaders/GammaCorrectionShader.js";
 import { gsap } from "gsap";
 import { showInfoDiv } from "./showInfoDiv.js"; // 导入 showInfoDiv 函数
+import { PerspectiveCameraForResizableWindow, handleCameraRotation, handleMouseMovement } from './CameraWithMouseRotation.js';
+import CameraOrientationState from './CameraOrientationState.js';
+import { PI } from "three/examples/jsm/nodes/Nodes.js";
 
-let scene, camera, renderer, composer, model, controls, raycaster, mouse;
+let scene, camera, renderer, composer, model, controls, raycaster;
 const objects = [];
+let cameraOrientationState = new CameraOrientationState();
 let lastCameraPosition = new THREE.Vector3();
 const button = document.getElementById("fixedButton");
 const backgroundVideo = document.getElementById("backgroundVideo");
@@ -22,6 +26,7 @@ const videoOverlay = document.getElementById("videoOverlay");
 const introVideo = document.getElementById("introVideo");
 const threeContainer = document.getElementById("three-container");
 const shopButton = document.getElementById("shopButton");
+const mouse = new THREE.Vector2();
 
 window.addEventListener("load", () => {
   // Notify iframe to start loading
@@ -48,9 +53,7 @@ introVideo.addEventListener("ended", () => {
 
 init();
 
-
 function init() {
-
   // Scene
   scene = new THREE.Scene();
 
@@ -67,19 +70,14 @@ function init() {
   const targetLookAt2 = new THREE.Vector3(-2.3, 1, -0.7);
 
   // Camera
-  camera = new THREE.PerspectiveCamera(
-    50,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    2000
-  );
+  camera = PerspectiveCameraForResizableWindow(30, 0.1, 10000, renderer);
   camera.position.set(5.04, 1.03, -1.53);
-  camera.lookAt(targetLookAt2);
-  lastCameraPosition.copy(camera.position);
-
+  camera.rotation.x = THREE.MathUtils.degToRad(10); // Example: 10 degrees
+  camera.rotation.y = THREE.MathUtils.degToRad(-90); // Example: 30 degrees
+  camera.rotation.z = THREE.MathUtils.degToRad(0); // Example: 0 degrees
+  
   // Raycaster
   raycaster = new THREE.Raycaster();
-  mouse = new THREE.Vector2();
 
   // Configure and load GLTF model with DRACOLoader
   const dracoLoader = new DRACOLoader();
@@ -164,7 +162,6 @@ function init() {
   // moveButton.addEventListener("click", onMoveButtonClick);
 }
 
-
 function updateButtonPosition() {
   const vector = new THREE.Vector3();
   vector.setFromMatrixPosition(model.matrixWorld);
@@ -212,50 +209,19 @@ function onMouseClick(event) {
 }
 
 function onMouseMove(event) {
-  // Define a sensitivity factor
-  const sensitivity = 0.01; // 调整这个值以改变灵敏度
-
-  // Normalize mouse coordinates to [-1, 1]
   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  mouse.y = (event.clientY / window.innerHeight) * 2 - 1;
 
-  // Convert normalized coordinates to world coordinates
-  const vector = new THREE.Vector3(mouse.x * sensitivity, mouse.y * sensitivity, 0.5); // 乘以灵敏度因子
-  vector.unproject(camera);
-  const dir = vector.sub(camera.position).normalize();
-  const distance = -camera.position.z / dir.z;
-  const pos = camera.position.clone().add(dir.multiplyScalar(distance));
-
-  // Limit pos within specific ranges
-  const minX = -1, maxX = 1;
-  const minY = -1, maxY = 1;
-  const minZ = -2, maxZ = 2;
-  
-  pos.x = Math.max(minX, Math.min(maxX, pos.x));
-  pos.y = Math.max(minY, Math.min(maxY, pos.y));
-  pos.z = Math.max(minZ, Math.min(maxZ, pos.z));
-
-  // Update camera lookAt
-  camera.lookAt(pos);
+  handleMouseMovement(mouse.x, mouse.y, cameraOrientationState);
 }
+document.addEventListener('mousemove', onMouseMove, false);
+
 
 
 function animate() {
   requestAnimationFrame(animate);
-  if (!camera.position.equals(lastCameraPosition)) {
-    updateMaterials();
-    lastCameraPosition.copy(camera.position);
-  }
-  
+  handleCameraRotation(camera, cameraOrientationState);
   // Render scene with composer
   composer.render();
   updateButtonPosition();
-}
-
-function updateMaterials() {
-  objects.forEach((child) => {
-    if (child.material) {
-      child.material.needsUpdate = true;
-    }
-  });
 }
